2d1f7141408e6b0ed3bd9696ea12451fac0234c6
KonH <konh@yandex.ru>
2020-11-23 18:55:19 +0700

    Fix codestyle

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <a v-bind:href="model.htmlUrl">{{ model.name }}</a> (language:
+    <a :href="model.htmlUrl">{{ model.name }}</a> (language:
     {{ model.language }}, stars: {{ model.stargazersCount }}, updated at:
     {{ model.updatedAt }})
     <p>{{ model.description }}</p>
@@ -10,7 +10,6 @@
 import { Vue } from "vue-class-component";
 import { Prop } from "vue-property-decorator";
 import ProjectModel from "@/model/ProjectModel";
-
 
 export default class Project extends Vue {
   @Prop()
